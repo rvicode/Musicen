0244d32ec9df8e968ccc78d69c50967ac835a4dc
Arvin Visi <arvinvisi03@gmail.com>
2024-07-13 20:53:52 +0330

    add color in components folder

---
+++ lib/components/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ThemeColor {
+  static const Color backgroundColor = Color(0xff051923);
+}
+
+class TextThemeColor {
+  static const Color mainTextColor = Color(0xff00C2CB);
+}
