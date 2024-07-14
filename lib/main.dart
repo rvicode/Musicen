7222e04d0c3bb80a1da963da847b4e6c58579661
Arvin Visi <arvinvisi03@gmail.com>
2024-07-14 19:17:55 +0330

    add another texTheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:musicen/components/colors.dart';
+import 'package:musicen/gen/fonts.gen.dart';
 import 'package:musicen/screens/home_screen/home.screen.dart';
 
 void main() {
@@ -10,30 +10,37 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var fontFamily = FontFamily.roboto;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-        textTheme: const TextTheme(
-          headlineLarge: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-            color: TextThemeColor.mainTextColor,
-          ),
-        ),
+        textTheme: TextTheme(
+            headlineLarge: TextStyle(
+              fontFamily: fontFamily,
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              color: TextThemeColor.mainTextColor,
+            ),
+            headlineMedium: TextStyle(
+                fontFamily: fontFamily,
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
+            bodyMedium: TextStyle(
+                fontFamily: fontFamily,
+                fontSize: 18,
+                color: Colors.white,
+                fontWeight: FontWeight.normal),
+            bodySmall: TextStyle(
+                fontFamily: fontFamily,
+                fontSize: 14,
+                color: Colors.white70,
+                fontWeight: FontWeight.w300)),
       ),
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('fa'), // Farsi
-      ],
       home: const HomeScreen(),
     );
   }
