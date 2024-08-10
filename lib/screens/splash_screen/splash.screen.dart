f6667e48fda214e0afd4b7d78f28e174f2937c42
Arvin Visi <arvinvisi03@gmail.com>
2024-08-10 20:53:57 +0330

    add splash_screen folder

---
+++ lib/screens/splash_screen/splash.screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:musicen/main.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3)).then(
+      (value) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const MainScreen(),
+          ),
+          (route) => false,
+        );
+      },
+    );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    var size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.black,
+        ),
+        Center(
+          child: Text(
+            'Musicen',
+            style: textTheme.headlineLarge,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: size.height / 1.2),
+          child: Center(
+            child: LoadingAnimationWidget.staggeredDotsWave(
+              color: Colors.grey,
+              size: 50,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
