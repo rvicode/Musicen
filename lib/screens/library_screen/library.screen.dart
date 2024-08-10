2b0e6e4ed92d799f0bced85e215e87225e614cd4
Arvin Visi <arvinvisi03@gmail.com>
2024-08-10 19:50:42 +0330

    add widgets for library_screen folder and make widgets for widgets folder

--- lib/screens/library_screen/library.screen.dart
+++ lib/screens/library_screen/library.screen.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:musicen/components/colors.dart';
 import 'package:musicen/fake_data/fake.data.dart';
+import 'package:musicen/screens/library_screen/widgets/add.new.play.list.dart';
+import 'package:musicen/screens/library_screen/widgets/like.songs.icon.dart';
 
 class LibScreen extends StatefulWidget {
   const LibScreen({super.key});
@@ -25,7 +26,6 @@ class _LibScreenState extends State<LibScreen> {
       child: Scaffold(
         backgroundColor: ThemeColor.backgroundColor,
         appBar: AppBar(
-          
           forceMaterialTransparency: true,
           toolbarHeight: size.height / 12,
           backgroundColor: ThemeColor.backgroundColor,
@@ -41,10 +41,13 @@ class _LibScreenState extends State<LibScreen> {
             ],
           ),
         ),
+
+        // Body
         body: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // Category List Bar
               Padding(
                 padding: const EdgeInsets.only(top: 16),
                 child: SizedBox(
@@ -96,65 +99,13 @@ class _LibScreenState extends State<LibScreen> {
                   ),
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(
-                    top: 32, left: size.width / 20, right: size.width / 3.5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      height: 56,
-                      width: 56,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          gradient: const LinearGradient(
-                              colors: [
-                                Color(0xff00C2CB),
-                                Color.fromRGBO(155, 243, 255, 1)
-                              ],
-                              begin: Alignment.bottomCenter,
-                              end: Alignment.topCenter)),
-                      child: const Center(
-                        child: Icon(Icons.add),
-                      ),
-                    ),
-                    Text(
-                      'Add New Playlist',
-                      style: textTheme.bodyLarge,
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                    top: 32, left: size.width / 20, right: size.width / 3.5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      height: 56,
-                      width: 56,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        gradient: const LinearGradient(
-                            colors: [
-                              Color(0xff00C2CB),
-                              Color.fromRGBO(155, 243, 255, 1),
-                            ],
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter),
-                      ),
-                      child: const Center(
-                        child: Icon(CupertinoIcons.heart),
-                      ),
-                    ),
-                    Text(
-                      'Your Liked Songs',
-                      style: textTheme.bodyLarge,
-                    ),
-                  ],
-                ),
-              ),
+
+              // Add New Play Lists Icon
+              AddNewPlayList(size: size, textTheme: textTheme),
+
+              // Like Songs Icon
+              LikeSongsIcon(size: size, textTheme: textTheme),
+
               Padding(
                 padding: const EdgeInsets.only(top: 32, left: 24),
                 child: Text(
---
+++ lib/screens/library_screen/widgets/add.new.play.list.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AddNewPlayList extends StatelessWidget {
+  const AddNewPlayList({
+    super.key,
+    required this.size,
+    required this.textTheme,
+  });
+
+  final Size size;
+  final TextTheme textTheme;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+          top: 32, left: size.width / 20, right: size.width / 3.5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            height: 56,
+            width: 56,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                gradient: const LinearGradient(
+                    colors: [
+                      Color(0xff00C2CB),
+                      Color.fromRGBO(155, 243, 255, 1)
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter)),
+            child: const Center(
+              child: Icon(Icons.add),
+            ),
+          ),
+          Text(
+            'Add New Playlist',
+            style: textTheme.bodyLarge,
+          ),
+        ],
+      ),
+    );
+  }
+}
